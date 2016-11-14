5ceab118a18c60f54127a9c43f8c0439f1cbf48c
Filipe Laíns <ffy00.gnc@gmail.com>
2016-11-14 11:54:41 +0000

    BETA 0.3
    - Add walk()
    - Add walk(int speed)
    - Add stop()
    - Updated file comment
    - Add @version parameter
    - Remove unused variables
    - Add basic core code

--- Core.ino
+++ Core.ino
@@ -1,59 +1,69 @@
-//   /\/\   ___  _ __  _______  _ __ __ _  | @repo firefighter-robot
-//  /    \ / _ \| '_ \|_  / _ \| '__/ _` | | Firefighter Robot firmware based on Arduino Board
-// / /\/\ \ (_) | | | |/ / (_) | | | (_| | | @license MIT License
-// \/    \/\___/|_| |_/___\___/|_|  \__, | | @authors "Luís Ferreira" <lsferreira169@gmail.com>
-//                                  |___/  | @authors "Filipe Laíns" <filipe.lains@gmail.com>
+/**
+     /\/\   ___  _ __  _______  _ __ __ _ 
+    /    \ / _ \| '_ \|_  / _ \| '__/ _` |
+   / /\/\ \ (_) | | | |/ / (_) | | | (_| |
+   \/    \/\___/|_| |_/___\___/|_|  \__, |
+                                    |___/ 
+  Firefighter Robot
+  Core.ino
+  Firefighter Robot firmware based on Arduino Board
+  
+  @repo firefighter-robot
+  @license MIT License
+  @authors "Filipe Laíns" <filipe.lains@gmail.com>
+  @authors "Luís Ferreira" <lsferreira169@gmail.com>
+  @version BETA 0.3 14/11/2016
+*/
 
-///External Libraries
+// External Libraries
 #include <FastLED.h>
-#include <SharpIR.h>
 
-///Includes
-#include "PitchesNotes.h"
-
-///Constants
+// Constants
 #define DATA_LED_PIN   13 // D13
 #define RGB_PIN        13 // D13
-#define MOTOR_LEFT     7  // D7 ///TODO: Know if it's correct!
-#define MOTOR_RIGHT    4  // D4 ///TODO: Know if it's correct!
-#define PX_F 0  // A0 ///Front Side
-#define PX_L 1  // A1 ///Left Side
-
-#define IR_PROXIMITY_MODEL 430 // GP2YA41SK0F ///TODO: Need to be confirmed!
+#define MOTOR_LEFT     7  // D7 - TODO: Know if it's correct!
+#define MOTOR_RIGHT    4  // D4 - TODO: Know if it's correct!
+#define PX_F 0  // A0 - Front Side
+#define PX_L 1  // A1 - Left Side
 #define NUM_RGB_LEDS   2
 
-///Objects
+// Objects
 CRGB rgb_strip[NUM_RGB_LEDS];
 
-void setup() {
-  // put your setup code here, to run once:
-  //pinMode(13, OUTPUT);
-  pinMode(9, OUTPUT);
-  FastLED.addLeds<NEOPIXEL, RGB_PIN>(rgb_strip, NUM_RGB_LEDS);
-  pinMode(MOTOR_LEFT, OUTPUT);
-  pinMode(MOTOR_RIGHT, OUTPUT);
-  digitalWrite(MOTOR_RIGHT, LOW);
-  pinMode(NUM_RGB_LEDS, INPUT);
-  Serial.begin(9600);
-}
-
-void loop() {
-    if (analogRead(SENSOR_FRONT) > 300){
-      if(analogRead(SENSOR_LEFT) > 150){
-          
-      } else {
-        
-      }
-    }
-    delay(200);
-}
-
+// Direction
 enum Direction{
   RIGHT,
   LEFT,
   FRONT
 };
 
+// Setup
+void setup() {
+  //pinMode(13, OUTPUT);
+  pinMode(9, OUTPUT);
+  FastLED.addLeds<NEOPIXEL, RGB_PIN>(rgb_strip, NUM_RGB_LEDS);
+  pinMode(MOTOR_LEFT, OUTPUT);
+  pinMode(MOTOR_RIGHT, OUTPUT);
+  pinMode(NUM_RGB_LEDS, INPUT);
+  Serial.begin(9600);
+}
+
+// Loop
+void loop() {
+    if (analogRead(SENSOR_FRONT) > 300){
+      if(analogRead(SENSOR_LEFT) > 150){
+          stop();
+          rotate();
+      } else {
+        stop();
+      }
+    } else {
+      walk();
+    }
+    delay(200);
+}
+
+// Rotate
 inline void rotate(Direction d){
   delay(300);
   switch(d){
@@ -72,7 +82,20 @@ inline void rotate(Direction d){
   }
 }
 
+// Walk
 inline void walk(){
-  analogWrite(MOTOR_RIGHT, 255);
-  analogWrite(MOTOR_RIGHT, 255);
+  digitalWrite(MOTOR_RIGHT, HIGH);
+  digitalWrite(MOTOR_LEFT, HIGH);
+}
+
+// Walk
+inline void walk(int speed){
+  analogWrite(MOTOR_RIGHT, speed);
+  analogWrite(MOTOR_LEFT, speed);
+}
+
+// Stop
+inline void stop(){
+  digitalWrite(MOTOR_RIGHT, LOW);
+  digitalWrite(MOTOR_LEFT, LOW);
 }
