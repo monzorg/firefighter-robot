6d5ce9eb4ae7479ebaae269b62e0f6dc833470f7
Filipe Laíns <ffy00.gnc@gmail.com>
2016-11-14 19:26:39 +0000

    BETA 0.5
    - Add Core.h
    - Fix enum declaration

--- Core.ino
+++ Core.ino
@@ -12,20 +12,12 @@
   @license MIT License
   @authors "Filipe Laíns" <filipe.lains@gmail.com>
   @authors "Luís Ferreira" <lsferreira169@gmail.com>
-  @version BETA 0.4 14/11/2016
+  @version BETA 0.5 14/11/2016
 */
 
 // External Libraries
 #include <FastLED.h>
-
-// Constants
-#define DATA_LED_PIN   13 // D13
-#define RGB_PIN        13 // D13
-#define MOTOR_LEFT     7  // D7 - TODO: Know if it's correct!
-#define MOTOR_RIGHT    4  // D4 - TODO: Know if it's correct!
-#define PX_F 0  // A0 - Front Side
-#define PX_L 1  // A1 - Left Side
-#define NUM_RGB_LEDS   2
+#include "Core.h"
 
 // Objects
 CRGB rgb_strip[NUM_RGB_LEDS];
