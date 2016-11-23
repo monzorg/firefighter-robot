8a1a75f6bda2513b7ba363d7b07d33edbb8f62ce
Filipe Laíns <ffy00.gnc@gmail.com>
2016-11-23 17:55:13 +0000

    Update Core.ino

--- Core.ino
+++ Core.ino
@@ -12,33 +12,35 @@
   @license MIT License
   @authors "Filipe Laíns" <filipe.lains@gmail.com>
   @authors "Luís Ferreira" <lsferreira169@gmail.com>
-  @version BETA 0.5 14/11/2016
+  @version ALPHA 0.5 23/11/2016
 */
 
 // External Libraries
 #include <FastLED.h>
-#include "Core.h"
+#include <SharpIR.h>
+//#include <Wire.h>
+
+#define DATA_LED_PIN 13
+#define RGB_PIN 13
+
+#define PX_F 0  // A0 - Front Side
+#define PX_L 1  // A1 - Left Side
+#define COMPASS_ADDRESS 0x60 //defines address of compass
+#define NUM_RGB_LEDS   2
+
+enum Direction {
+  RIGHT,
+  LEFT
+};
 
 // Objects
 CRGB rgb_strip[NUM_RGB_LEDS];
-
-// Direction
-enum Direction{
-  RIGHT,
-  LEFT,
-  FRONT
-};
 
 /**
      Setup Code
 */
 void setup() {
-  //pinMode(13, OUTPUT);
-  pinMode(9, OUTPUT);
-  FastLED.addLeds<NEOPIXEL, RGB_PIN>(rgb_strip, NUM_RGB_LEDS);
-  pinMode(MOTOR_LEFT, OUTPUT);
-  pinMode(MOTOR_RIGHT, OUTPUT);
-  pinMode(NUM_RGB_LEDS, INPUT);
+  Motor m(7, 4, 6, 5);
   Serial.begin(9600);
 }
 
@@ -46,67 +48,5 @@ void setup() {
      Loop Code
 */
 void loop() {
-    if (analogRead(PX_F) > 300){
-      if(analogRead(PX_L) > 150){
-          stop();
-          rotate(RIGHT);
-          walk();
-      } else {
-          stop();
-          rotate(LEFT);
-          walk();
-      }
-    } else {
-      walk();
-    }
-    delay(200);
-}
-
-/**
-     Rotates
-     
-     @param direction
-*/
-inline void rotate(Direction d){
-  delay(300);
-  switch(d){
-    case RIGHT:
-      digitalWrite(MOTOR_RIGHT, HIGH);
-      digitalWrite(MOTOR_LEFT, LOW);
-      break;
-    case LEFT:
-      digitalWrite(MOTOR_RIGHT, LOW);
-      digitalWrite(MOTOR_LEFT, HIGH);
-      break;
-    case FRONT:
-      digitalWrite(MOTOR_RIGHT, HIGH);
-      digitalWrite(MOTOR_LEFT, HIGH);
-      break;
-  }
-}
-
-/**
-     Walks
-*/
-inline void walk(){
-  digitalWrite(MOTOR_RIGHT, HIGH);
-  digitalWrite(MOTOR_LEFT, HIGH);
-}
-
-/**
-     Walks
-     
-     @param speed
-*/
-inline void walk(int speed){
-  analogWrite(MOTOR_RIGHT, speed);
-  analogWrite(MOTOR_LEFT, speed);
-}
-
-/**
-     Stops
-*/
-inline void stop(){
-  digitalWrite(MOTOR_RIGHT, LOW);
-  digitalWrite(MOTOR_LEFT, LOW);
+  
 }
